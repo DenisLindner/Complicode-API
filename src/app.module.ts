6f833ae17c81708c511cd9af23185ade517cf63a
Denis Lindner <lindnerdenis19@gmail.com>
2026-04-09 14:30:24 -0300

    feat: helmet and rate limit configured

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,19 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { AuthTokenGuard } from './auth/guards/auth-token.guard';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
     PrismaModule,
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000,
+          limit: 10,
+        },
+      ],
+    }),
     UsersModule,
     AuthModule,
     ConfigModule.forRoot({ isGlobal: true }),
@@ -17,6 +26,10 @@ import { ConfigModule } from '@nestjs/config';
     {
       provide: 'APP_GUARD',
       useClass: AuthTokenGuard,
+    },
+    {
+      provide: 'APP_GUARD',
+      useClass: ThrottlerGuard,
     },
   ],
 })
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,7 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  app.use(helmet());
   app.enableCors({
     origin: ['http://localhost:3000'],
   });
